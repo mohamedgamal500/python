b99fd06f8a42c8199693bdcc6840d95376a5d2e7
jemy <mohamedgamalmohamed800@gmail.com>
2020-06-20 20:38:53 +0200

    py

--- files/with.py
+++
@@ -1,6 +0,0 @@
-'''same as fileobj = open('jemy.txt','r') as fileobj 
-    and you dont need to use close file'''
-with open('jemy.txt', 'r') as fileobj:
-        lines = fileobj.readlines()
-        for line in lines[:]:
-            print(line.strip())
